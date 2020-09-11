dd20071ac0d2fa2ffe40ed5946cf53b839211519
Shane Neuville <shneuvil@microsoft.com>
2020-09-11 16:53:04 -0500

    Shell FlyoutWidth and FlyoutHeight

--- Xamarin.Forms.Platform.UAP/Shell/ShellRenderer.cs
+++ Xamarin.Forms.Platform.UAP/Shell/ShellRenderer.cs
@@ -25,6 +25,9 @@ namespace Xamarin.Forms.Platform.UWP
 		internal const string ShellStyle = "ShellNavigationView";
 		Shell _shell;
 		Brush _flyoutBackdrop;
+		double _flyoutHeight = -1d;
+		double _flyoutWidth = -1d;
+
 		FlyoutBehavior _flyoutBehavior;
 		List<List<Element>> _flyoutGrouping;
 		ShellItemRenderer ItemRenderer { get; }
@@ -77,6 +80,7 @@ namespace Xamarin.Forms.Platform.UWP
 			UpdatePaneButtonColor(NavigationViewBackButton, false);
 			UpdateFlyoutBackgroundColor();
 			UpdateFlyoutBackdrop();
+			UpdateFlyoutPosition();
 			UpdateFlyoutVerticalScrollMode();
 		}
 
@@ -241,6 +245,20 @@ namespace Xamarin.Forms.Platform.UWP
 			}
 		}
 
+		void UpdateFlyoutPosition()
+		{
+			if (_flyoutBehavior == FlyoutBehavior.Disabled)
+				return;
+
+			var splitView = ShellSplitView;
+			if (splitView != null)
+			{
+				splitView.SetFlyoutSizes(_flyoutHeight, _flyoutWidth);
+				if (IsPaneOpen)
+					ShellSplitView.RefreshFlyoutPosition();
+			}
+		}
+		
 		void UpdateFlyoutBackdrop()
 		{
 			if (_flyoutBehavior != FlyoutBehavior.Flyout)
@@ -251,7 +269,7 @@ namespace Xamarin.Forms.Platform.UWP
 			{
 				splitView.FlyoutBackdrop = _flyoutBackdrop;
 				if (IsPaneOpen)
-					ShellSplitView.UpdateFlyoutBackdrop();
+					ShellSplitView.RefreshFlyoutBackdrop();
 			}
 		}
 
@@ -418,6 +436,9 @@ namespace Xamarin.Forms.Platform.UWP
 					titleColor = appearance.TitleColor.ToWindowsColor();
 
 				_flyoutBackdrop = appearance.FlyoutBackdrop;
+
+				_flyoutWidth = appearance.FlyoutWidth;
+				_flyoutHeight = appearance.FlyoutHeight;
 			}
 
 			var titleBar = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().TitleBar;
@@ -425,8 +446,8 @@ namespace Xamarin.Forms.Platform.UWP
 			titleBar.ForegroundColor = titleBar.ButtonForegroundColor = titleColor;
 			UpdatePaneButtonColor(TogglePaneButton, !IsPaneOpen);
 			UpdatePaneButtonColor(NavigationViewBackButton, !IsPaneOpen);
-
 			UpdateFlyoutBackdrop();
+			UpdateFlyoutPosition();
 		}
 
 		#endregion IAppearanceObserver
