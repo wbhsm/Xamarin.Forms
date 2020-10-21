d9570bec279f10d157a3b97989d22cac0d122d63
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-10-21 14:51:20 +0200

    Added Switch.Impl.cs

---
+++ src/Forms/src/Xamarin.Forms.Core/HandlerImpl/Switch.Impl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Platform;
+
+namespace Xamarin.Forms
+{
+	public partial class Switch : ISwitch
+	{
+		void ISwitch.Toggled()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
